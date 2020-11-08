3f54a4ef1c1dd8368a7081729d52cebc3276529b
Kmrunal20 <mrunal.kurhade@digiplusit.com>
2020-11-09 00:05:45 +0530

    [mrunal] Refactor. Modified file names

--- AddressBookProblemUsingLinq/AddressBook.cs
+++ AddressBookProblemUsingLinq/AddressBook.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace AddressBookProblemUsingLinq
+namespace AddressBookSystemProblem
 {
     public class AddressBook
     {
--- AddressBookProblemUsingLinq/Program.cs
+++ AddressBookProblemUsingLinq/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace AddressBookProblemUsingLinq
+namespace AddressBookSystemProblem
 {
     class Program
     {
